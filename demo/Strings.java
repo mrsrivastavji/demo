61c089b4d2f48fb8805ad1e15bb075bb22813cd7
Shivam Srivastav <107093652+mrsrivastavji@users.noreply.github.com>
2025-02-26 08:50:27 +0000

    done

---
+++ demo/Strings.java
@@ -0,0 +1,27 @@
+import java.util.*;
+// Strings are immutable
+public class Strings {
+    public static void main(String[] args) {
+        String name= "shivam srivastav";
+        String fullName= "Shivam Srivastav";
+        String sentance = "My name is Shivam";
+
+        Scanner sc= new Scanner(System.in);
+        String fName=sc.nextLine();
+        System.out.println(name);
+        System.out.println(sentance);
+        System.out.println(sentance.substring(15,20));
+        System.out.println(fName);
+        System.out.println(fName.length());
+        System.out.println(name + " " + sentance);
+        for(int i=0; i<fullName.length(); i++){
+            System.out.println(fullName.charAt(i));
+        }
+        
+        if(name.compareTo(fullName)==0){
+            System.out.println("Both name are same");
+        }
+        else{
+            System.out.println("Both name are not same");
+        }
+}
