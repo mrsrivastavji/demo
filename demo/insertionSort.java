01b81c6a04e0c617024e52a56d146022816722f4
Shivam Srivastav <107093652+mrsrivastavji@users.noreply.github.com>
2025-02-27 12:01:57 +0000

    done

---
+++ demo/insertionSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Sorting{
+    public static void main(String args[]){
+        System.out.print("Enter length of array: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int A[]=new int[n];
+         System.out.print("Enter element of array: ");
+        for(int i=0; i<n; i++){
+            A[i]=sc.nextInt();
+        }
+        
+        for(int i=0; i<n; i++){
+            int cur=A[i];
+            int j=i-1;
+            while(j>=0 && cur<A[j]){
+                A[j+1]=A[j];
+                j--;
+            }
+            A[j+1]=curr;
+        }
+        System.out.print("Sorted element of array: ");
+        for(int i=0; i<n; i++){
+            System.out.print(A[i]+" ");
+        }
+    }
+}
