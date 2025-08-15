a2ad08a7346316c507a99a648096fa8e9cea1f34
Shivam Srivastav <107093652+mrsrivastavji@users.noreply.github.com>
2025-08-15 08:18:01 +0000

    done

--- demo/pattern.java
+++ demo/pattern.java
@@ -2,80 +2,80 @@ import java.util.*;
 
 public class pattern{
     public static void main(String arg[]){
-        // Scanner sc= new Scanner(System.in);
-        // System.out.print("Enter no of rows: ");
-        // int row=sc.nextInt();
-        // System.out.print("Enter no of columns: ");
-        // int col=sc.nextInt();
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter no of rows: ");
+        int row=sc.nextInt();
+        System.out.print("Enter no of columns: ");
+        int col=sc.nextInt();
 
-        // for(int i= 1; i<=row; i++){
-        //     for(int j=1; j<=col; j++){
-        //         //System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i= 1; i<=row; i++){
+            for(int j=1; j<=col; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        // for(int i=1; i<=4; i++)
-        // {
-        //     for(int j=1; j<=5; j++){
-        //         if(i==1|| i==4 || j==1|| j==5){
-        //             System.out.print("*");
-        //         }
-        //         else{
-        //             System.out.print(" ");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=1; i<=4; i++)
+        {
+            for(int j=1; j<=5; j++){
+                if(i==1|| i==4 || j==1|| j==5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
 
-        // for(int i=1; i<=5; i++){
-        //     for(int j=1; j<=i; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        // for(int i=5; i>0; i--){
-        //     for(int j=i; j>0; j--){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=5; i>0; i--){
+            for(int j=i; j>0; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        // for(int i=1; i<=5; i++){
-        //     for(int j=1; j<=5; j++){
-        //         if(5-i>=j){
-        //             System.out.print(" ");
-        //         }
-        //         else{
-        //             System.out.print("*");
-        //         }
-        //     }
-        //     System.out.println();
-        //}
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
+                if(5-i>=j){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
 
-        // for(int i=1; i<=5; i++){
-        //     for(int j=1; j<=i; j++){
-        //         System.out.print(j);
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
 
-        // for(int i=5; i>0; i--){
-        //     for(int j=1; j<=i; j++){
-        //         System.out.print(j);
-        //     }
-        //     System.out.println();
-        // }
-        // int k=1;
-        // for(int i=1; i<=5; i++){
-        //         for(int j=1; j<=i; j++){
-        //             System.out.print(k+" ");
-        //             k++;
-        //         }
-        //         System.out.println();
-        //     }
+        for(int i=5; i>0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        int k=1;
+        for(int i=1; i<=5; i++){
+                for(int j=1; j<=i; j++){
+                    System.out.print(k+" ");
+                    k++;
+                }
+                System.out.println();
+            }
 
         for(int i=1; i<=5; i++){
             for(int j=1; j<=i; j++){
@@ -87,5 +87,7 @@ public class pattern{
             }
             System.out.println();
         }
+
+        
     }
 }
