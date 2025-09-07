92f0b7712df7678df0b281e157b06c47a7e1f906
Shivam Srivastav <107093652+mrsrivastavji@users.noreply.github.com>
2025-09-07 00:08:02 +0000

    done

--- demo/Funcion.java
+++ demo/Funcion.java
@@ -58,10 +58,9 @@ public class Funcion{
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
         int n= sc.nextInt();
         int fact= fact(n);
         System.out.println("Factorial is: "+ fact);
-        sc.close();
-
     }
 }
