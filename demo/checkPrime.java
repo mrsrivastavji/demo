4028ca486b72c0f48dae9bd0a39419c0625d36f4
Shivam Srivastav <107093652+mrsrivastavji@users.noreply.github.com>
2025-09-08 00:15:44 +0000

    done

--- demo/checkPrime.java
+++ demo/checkPrime.java
@@ -1,8 +1,12 @@
 import java.util.*;
 
-public class checkPrime {
+class checkPrime {
     static boolean isPrime(int n) {
-        for(int i=2; i<n/2; i++){
+        if (n <= 1)
+            return false;
+        if (n == 2) 
+            return true;
+        for(int i=2; i<=Math.sqrt(n); i++){
             if(n%i==0)
                 return false;
         }
